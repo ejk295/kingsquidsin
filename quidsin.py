1c4370c31060bce69b4eba592a02d25040a2a6d5
ejk295 <elliskingsport@outlook.com>
2026-06-10 22:36:24 +0100

    Update quidsin.py

--- quidsin.py
+++ quidsin.py
@@ -673,35 +673,31 @@ else:
                                 """, unsafe_allow_html=True)
                         
                         # --- IN-GROUP PLAYERS SUB-SECTION ---
-                        # --- IN-GROUP PLAYERS SUB-SECTION ---
-                    active_cards = []
+                        active_cards_html = "" # Initialize as a string
 
-                    for team_name in teams_in_group:
-                        if team_name in GROUP_PLAYERS:
-                            p_data = GROUP_PLAYERS[team_name]
-                            active_cards.append(f"""
+                        for team_name in teams_in_group:
+                            if team_name in GROUP_PLAYERS:
+                                p_data = GROUP_PLAYERS[team_name]
+                                # Add each card to the HTML string
+                                active_cards_html += f"""
                                 <div class="group-player-card">
-                                    <img 
-                                        src="{p_data['img_url']}" 
-                                        loading="eager" 
-                                        referrerpolicy="no-referrer" 
-                                        crossorigin="anonymous"
-                                    >
+                                    <img src="{p_data['img_url']}" loading="eager" referrerpolicy="no-referrer" crossorigin="anonymous">
                                     <div class="group-player-card-name">{p_data['player_name']}</div>
                                     <div class="group-player-card-team">{team_name}</div>
                                 </div>
-                            """)
+                                """
 
-                    if active_cards:
-                        st.markdown(
-                            "<div style='margin: 6px 0px 0px 0px !important;'><span style='font-size:12px; font-weight:700; color:#006847;'>🌟 Key Group Players</span></div>",
-                            unsafe_allow_html=True
-                        )
-                        players_row_html = '<div class="group-players-container">' + "".join(active_cards) + '</div>'
-                        st.markdown(players_row_html, unsafe_allow_html=True)                        
-
-                    # Close the group-row-spacer div
-                    st.markdown('</div>', unsafe_allow_html=True)
+                        # Only render if we actually found players
+                        if active_cards_html:
+                            st.markdown(
+                                "<div style='margin: 6px 0px 0px 0px !important;'><span style='font-size:12px; font-weight:700; color:#006847;'>🌟 Key Group Players</span></div>",
+                                unsafe_allow_html=True
+                            )
+                            # Render the full container with all cards at once
+                            st.markdown(f'<div class="group-players-container">{active_cards_html}</div>', unsafe_allow_html=True)
+                        
+                        # Close the group-row-spacer div
+                        st.markdown('</div>', unsafe_allow_html=True)
 
         # --- OVERPERFORMANCE LEADERBOARD ---
         st.markdown("<hr style='margin:30px 0px 20px 0px; border-top: 3px solid #006847;'>", unsafe_allow_html=True)
